350af9faff72bc9f6f39757f893ef1984e5699a8
fyodo <fyodoorwork@gmail.com>
2025-10-14 13:10:36 +0300

    third

---
+++ src/lesson2/GreetingProgram.java
@@ -0,0 +1,18 @@
+package lesson2;
+
+import java.util.Scanner;
+
+public class GreetingProgram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.println("Enter your age: ");
+        int age = sc.nextInt();
+
+        System.out.println("Your name - " + name + ", age - " + age);
+    }
+
+}
